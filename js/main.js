cb237bed080ccbf71e4358e7eccef9d45463b6c4
shiduu <abdirashid324@gmail.com>
2020-08-04 08:49:11 +0300

    2nd commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'shidu'
+        name: 'shidu',
+        anchor: 'https://www.google.com',
+        link: '<a href="https://gaana.com/">songs</a>'
+    },
+    methods: {
+        changeName: function(){
+            this.name = event.target.value
+        },
+        returnName: function(){
+            return this.name
+        }
     }
 })
